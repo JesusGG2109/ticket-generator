3ecf547a75d00df6144b135dd17ae9ab6d3a468c
JesusGG2109 <22030773@itcelaya.edu.mx>
2026-05-12 15:23:14 -0600

    Reestructura inicial fullstack del proyecto

---
+++ client/src/components/ticket-form-page/ticket-form-page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from './hero'
+import { Form } from './form/form'
+
+export const TicketFormPage = () => {
+  return (
+    <section className='mx-auto pb-[76px]'>
+      <Hero />  
+      <Form />  
+    </section>
+  )
+}
---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { ShowTicketProvider } from './contexts/show-ticket-provider'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ShowTicketProvider>
+      <App />
+    </ShowTicketProvider>
+  </StrictMode>,
+)
